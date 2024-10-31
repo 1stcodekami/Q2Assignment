0ba82716537406aa109436f3dee732c1eb237e4e
1stcodekami <mkkhurshid33@gmail.com>
2024-10-31 11:08:29 -0700

    Class-1 Assignment

---
+++ Class_01/src/app/page.tsx
@@ -0,0 +1,12 @@
+//Quarter-2 Class-1 Assignment:
+//  Create a new project of NextJS with the CLI - and code a simple hello world program in it.
+
+
+export default function home(){
+  return (
+    <div>
+      <h1>Hello world...!</h1>
+    </div>
+  )
+
+}
