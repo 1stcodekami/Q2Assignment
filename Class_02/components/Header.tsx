32bb3b5213bddeb5ba260c40d57fcf213063cbb6
1stcodekami <mkkhurshid33@gmail.com>
2024-11-02 17:51:36 -0700

    Class-2 Assignment

---
+++ Class_02/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <h1> Home </h1>
+               
+      </header>
+    </div>
+  )
+}
+
+export default Header
